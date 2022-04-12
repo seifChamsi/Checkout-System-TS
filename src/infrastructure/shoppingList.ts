9baab42f937c99c28e7b38ecd6eedfcbf6662bbb
seifChamsi <saif.chamsi99@gmail.com>
2022-04-13 00:53:06 +0200

    :rocket: Add infrastructure/shoppingList

---
+++ src/infrastructure/shoppingList.ts
@@ -0,0 +1,29 @@
+import Product from "../contracts/Product";
+
+export const shoppingList: Product[] = [
+  {
+    id: "001",
+    name: "Pizza",
+    price: 18.0,
+  },
+  {
+    id: "002",
+    name: "T-shirt",
+    price: 45.0,
+  },
+  {
+    id: "003",
+    name: "Chips",
+    price: 6.0,
+  },
+  {
+    id: "001",
+    name: "Pizza",
+    price: 18.0,
+  },
+  {
+    id: "004",
+    name: "Cola",
+    price: 3.5,
+  },
+];
